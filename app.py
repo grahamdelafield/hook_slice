c03f81d097c03c1eca8313b467d68835d64804b9
Graham <delafield@wisc.edu>
2021-12-22 14:48:16 -0600

    adding Pipfile

--- app.py
+++ app.py
@@ -45,23 +45,8 @@ def track():
 @app.route('/analyze', methods=['GET', 'POST'])
 def form_example():
     # handle the POST request
-    if request.method == 'POST':
-        language = request.form.get('language')
-        framework = request.form.get('framework')
-        return '''
-                  <h1>The language value is: {}</h1>
-                  <h1>The framework value is: {}</h1>'''.format(language, framework)
+    return render_template('analyze.html')
 
-    # otherwise handle the GET request
-    return '''
-        <form method="POST">
-            <div><label>Language: <input type="text" name="language"></label></div>
-            <div><label>Framework: <input type="text" name="framework"></label></div>
-            <input type="submit" value="Submit">
-        </form>'''
-
-def my_print():
-    print('HATE')
 
 # if __name__=='__main__':
 #     app.run(debug=True)
